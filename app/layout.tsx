722e96d8808fecf559fbc7330c58e91ab28d5b54
abd.rehman <abd.rehman@ilmux.com>
2025-07-16 00:39:35 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Portfolio',
+  description: 'Created with Abdul Rehman',
+  generator: 'Next,js',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
